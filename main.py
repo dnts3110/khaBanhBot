8245d1dbe78138895f842065d68efee26bc72651
dnts3110 <65474495+dnts3110@users.noreply.github.com>
2021-07-17 01:11:59 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -65,10 +65,11 @@ async def on_message(message):
                f"Người ta bảo nghĩ gì thì uốn lưỡi 7 lần trước khi nói, riêng mày, {username}, lưỡi chó nói đéo khác sủa gâu gâu",
                f"Tổ sư bố thằng {username}, mịe mày lễ phép mày đâu? Dcmm thằng ăn bốc đéo ăn đũa",
                f"Ngô Bá Khá từ lúc lọt từ lồn ra đến nay, mới được tận mắt thấy một thanh niên mặc váy đái ngồi, "
-               f"mồm ngậm cứt {username}"]
+               f"mồm ngậm cứt {username}", "Vãi cả lồn bây giờ chó còn biết gõ bàn phím sủa gâu gâu"]
     randNum = random.randrange(0, len(chuibay))
     strings = user_message.lower()
-    words = ['dcm', 'đcm', 'con', 'dm', 'dmm', 'dit', 'me', 'ma', 'du', 'đm', 'đmm', 'địt', 'mẹ', 'má', 'đụ', 'long', 'lon', 'lông', 'lồn', 'dcmm', 'đcm', 'banh',
+    words = ['dcm', 'đcm', 'con', 'dm', 'dmm', 'dit', 'me', 'ma', 'du', 'đm', 'đmm', 'địt', 'mẹ', 'má', 'đụ', 'long',
+             'lon', 'lông', 'lồn', 'dcmm', 'đcm', 'banh',
              'bảnh']
     stringlist = strings.split()
     word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11, word12, word13, word14, word15, word16, word17, word18, word19, word20, word21, word22, word23 = words
@@ -76,9 +77,60 @@ async def on_message(message):
     if (
             word1 in stringlist or word2 in stringlist or word3 in stringlist or word4 in stringlist or word5 in stringlist or word6 in stringlist or word7 in stringlist or word8 in stringlist or word9 in stringlist or word10 in stringlist or word11 in stringlist or word12 in stringlist or word13 in stringlist or word14 in stringlist or word15 in stringlist or word16 in stringlist or word17 in stringlist or word18 in stringlist or word19 in stringlist or word20 in stringlist or word21 in stringlist) and (
             word22 in stringlist or word23 in stringlist):
-        print(True)
         await message.channel.send((chuibay[randNum]))
         randNum = random.randrange(0, len(chuibay))  # make the number random again
+        return
+
+    dongvien = [f"Đâu sẽ có đó, Bảnh tin em zai sẽ làm được, cố lên nhé em {username}", f"Cố lên {username}, em có "
+                                                                                        f"thể làm được mà. Bảnh sẽ "
+                                                                                        f"luôn bên cạnh hỗ trợ, "
+                                                                                        f"giúp đỡ em zai bằng hết khả "
+                                                                                        f"năng của mình. Cuộc đời của "
+                                                                                        f"em zai thì hãy tự tin “dám "
+                                                                                        f"nghĩ dám làm”. Sai thì sửa. "
+                                                                                        f"Ngu quá thì vào tù cũng "
+                                                                                        f"được", f"Trứng vịt còn lộn "
+                                                                                                 f"huống chi là con "
+                                                                                                 f"người. Có vấp ngã, "
+                                                                                                 f"sai lầm mới có "
+                                                                                                 f"thành công. Em zai {username} "
+                                                                                                 f"hãy cứ vững tin và "
+                                                                                                 f"bước tiếp thành "
+                                                                                                 f"công sẽ đến.",
+                f"Cuộc sống có lúc thăng trầm nhưng hãy luôn vững tin vào bản thân để đối diện với nó. Hãy biến chúng "
+                f"thành những bài học quý giá giúp ta trưởng thành hơn. Hãy vui lên và nhìn về phía trước.", f"Buồn "
+                                                                                                             f"hay áp "
+                                                                                                             f"lực "
+                                                                                                             f"quá "
+                                                                                                             f"thì "
+                                                                                                             f"khóc "
+                                                                                                             f"cũng "
+                                                                                                             f"được "
+                                                                                                             f"em zai "
+                                                                                                             f"à, "
+                                                                                                             f"rồi "
+                                                                                                             f"mọi "
+                                                                                                             f"chuyện "
+                                                                                                             f"sẽ qua "
+                                                                                                             f"thôi",
+                f"Động viên cái đéo gì ?? Địt mẹ mày tao nói một câu thôi, tập trung vào. Tắt mẹ discord đi dcmm",
+                f"Ngô Bá Khá rất tiếc nhưng mà động viên cái đầu buồi", f"Đằng nào cũng thất bại thôi em zai "
+                f"{username}", "Đời có bao nhiêu đâu mà buồn, hãy vững tin và quên đi tất cả. Cuộc sống có vô vàng "
+                "những điều tốt đẹp đang chờ em zai ở phía trước, vui vẻ đón nhận em zai sẽ thấy thật hạnh phúc và bình "
+                "yên.", f"Thành công sẽ không bao giờ từ chối những con người dám nghĩ dám làm. Ngô Bá Khá tin em {username} sẽ làm được tốt hơn thế.",
+                "Sau cơn mưa bầu trời sẽ xuất hiện cầu vồng đấy. Nỗi buồn cũng vậy sẽ qua đi và hãy mạnh mẽ để chào đón tương lai phía trước.",
+                f"Một ngày trôi qua là một ngày đến gần hơn với cái chết. Cho nên em zai {username} hãy tận dụng từng giây phút của mình để trải nghiệm và cải thiện bản thận nhé, Bảnh tin {username} sẽ thành công",
+                f"{username} à, Bảnh tin là em zai sẽ đạt được mọi mục đích, cố lên nha đcm em", "Không có con đường nào trải đầy hoa hồng và không có thành công nào là không có thất bại. Đó chỉ là những thử thách giúp em zai mài dũa sự cứng rắn và giúp em zai tỏa sáng hơn mà thôi.",
+                f"Làm một cốc nước lọc đi em zai, rồi hít một hơi thật sâu, em zai sẽ bình tĩnh và tập trung hơn đó, Bảnh biết em zai {username} làm được", "Mọi người có thể quay lưng với em zai, nhưng mà có một người em zai không được cho phép người đấy mất niềm tin. \n Đó chính là bản thân em zai, nếu mình còn không tin bản thân thì ai còn tin mình được nữa", f"Bảnh tin em zai {username} sẽ làm được điều vĩ đại trong tương lai \n Không tin Bảnh à? \n Hãy nhớ lại quá khứ khi em zai chưa sinh ra \n chính em zai là người vượt qua hàng trăm chục triệu thằng đầu buồi khác chứ k phải ai khác", f"Cố lên nha em zai {username}"]
+    rand_dong_vien = random.randrange(0, len(dongvien))
+    words2 = ['dong', 'vien', 'động', 'viên', 'buồn', 'banh', 'bảnh']
+    word_1, word_2, word_3, word_4, word_5, word_6, word_7 = words2
+    if (
+            word_1 in stringlist or word_2 in stringlist or word_3 in stringlist or word_4 in stringlist or word_5 in stringlist) and (
+            word_6 in stringlist or word_7 in stringlist):
+        print(True)
+        await message.channel.send((dongvien[rand_dong_vien]))
+        rand_dong_vien = random.randrange(0, len(dongvien))  # make the number random again
         return
 
     dieunhay = random.randrange(1, 6)
